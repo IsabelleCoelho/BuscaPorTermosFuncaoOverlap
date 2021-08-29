ac3edb6b13f1e7bc74b726fbdfb77cc42d6f8b2d
IsabelleCoelho <isabellerc37@gmail.com>
2021-08-29 16:14:34 -0300

    Finalizado!

--- freq_search.py
+++ freq_search.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import glob
 from os import chdir, listdir
 
 
@@ -11,15 +10,37 @@ class Noticia:
 
 def extracaoDasNoticias():
     arquivos = [arq for arq in listdir() if os.path.isfile(arq) and arq.lower().endswith(".txt")]
-    print(arquivos)
     noticias = []
     for noticia in arquivos:
-        noticias = Noticia(noticia)
+        noticias.append(Noticia(noticia))
     return noticias
 
 def preProcessamento():
     listaNoticias = extracaoDasNoticias()
-    
+    for noticia in listaNoticias:
+        with open(noticia.nomeArquivo, 'r') as arquivo:
+            texto = arquivo.read()
+            texto = texto.lower()
+            texto = list(filter(lambda c: c not in '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~', texto))
+            texto = "".join(texto) #convertendo de volta para uma string
+            texto = texto.replace("\n", " ")
+            noticia.listaTokens = texto.split(" ")
+    return listaNoticias
+
+def calculoOverlap(termos, noticiasPreproocessadas):
+    listaTermos = termos.split(" ")
+    arquivo = " "
+    maiorOverlap = -1
+    for noticia in noticiasPreproocessadas:
+        repeticoes = 0
+        for termo in listaTermos:
+            repeticoes += noticia.listaTokens.count(termo)
+        maiorLista = len(noticia.listaTokens) if len(noticia.listaTokens) > len(listaTermos) else len(listaTermos)
+        overlap = repeticoes/maiorLista
+        if overlap > maiorOverlap:
+            arquivo = noticia.nomeArquivo
+            maiorOverlap = overlap
+    return arquivo
 
 
 if __name__ == "__main__":
@@ -28,7 +49,10 @@ if __name__ == "__main__":
 
     if(os.path.exists(caminhoDiretorio)):
         chdir(caminhoDiretorio)
-        preProcessamento()
+        noticiasPreproocessadas = preProcessamento()
+        resultado = calculoOverlap(termos, noticiasPreproocessadas)
+        with open(resultado, 'r') as texto:
+            print(texto.read())
 
     else:
         print("Caminho para diretório inválido. Por favor, tente novamente!")
